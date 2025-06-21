094f7e0fc5ca22a532ff73f20e4d1a65901f05b9
w77sh <iraqxgmar915@gmail.com>
2025-06-22 00:19:44 +0300

    Initial Commit

---
+++ SwiftUi_Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi_Weather
+//
+//  Created by AbdulRahman Habeeb on 22/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi_Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi_Weather
+//
+//  Created by AbdulRahman Habeeb on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
